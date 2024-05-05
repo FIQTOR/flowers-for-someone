7af22234a8e9053e2e4df71b2e83e5ed2398451e
FIQTOR <yttaufiq190@gmail.com>
2024-05-05 15:10:58 +0700

    1.0.0 => [Frist Releases]

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const title = document.querySelector('.title')
+const text = 'I Have Something'.split('')
+for (let index = 0; index < text.length; index++) {
+  if (text[index] !== ' ') {
+    title.innerHTML += `<span>${text[index]}<span/>`
+  } else {
+    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+  }
+}
+
+const textElements = document.querySelectorAll('.title span');
+textElements.forEach((element) => {
+  const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
+  element.style.animationDelay = `${randomDelay}s`;
+});
