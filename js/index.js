e1fbeb33c4c6c11b2b9e6bf76037d2e22233e5ff
Maxirawan <irwanfahe@gmail.com>
2024-08-21 10:36:37 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'Feby Skizoo'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
