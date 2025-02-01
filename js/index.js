e9be1df47c53e36a8409a32c04301687da849f4f
bell <kb2988819@gmail.com>
2025-01-31 18:12:46 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'holaa aly'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
