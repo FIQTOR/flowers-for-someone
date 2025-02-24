5385a87caa7b7f8f80def0c9b9da9d37d3745ef2
Alinalove555 <luberpangsit@gmail.com>
2025-02-24 23:35:44 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('Ayiii I Love You').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
