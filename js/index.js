9b5a6d1b8ffb433d1c3ab70b03294dc8607153b9
FIQTOR <yttaufiq190@gmail.com>
2025-05-07 21:05:09 +0700

    frist deploy

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = `from nn cutes n pretty to dili lemon`.split('')
+
+// Create container for better responsive layout
+title.style.display = 'flex'
+title.style.flexWrap = 'wrap'
+title.style.justifyContent = 'center'
+title.style.gap = '0.5rem'
+
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}<span/>`
+    title.innerHTML += `<span>${text[index]}</span>`
   } else {
-    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+    title.innerHTML += `<span style='width: 1rem'></span>`
   }
 }
 
 const textElements = document.querySelectorAll('.title span');
 textElements.forEach((element) => {
-  const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
+  const randomDelay = Math.random() * 3;
   element.style.animationDelay = `${randomDelay}s`;
 });
